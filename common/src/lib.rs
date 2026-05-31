5f002974373fcfb613542c55f1190f747070cf8c
rangeset <Rangeset24@outlook.com>
2026-05-31 22:22:59 +0800

    docs(common): add header

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,9 @@
+// =====================================================================
+// Copyright (c) 2026 Phiarc Team and St Rangeset
+// Licensed under the GNU General Public License Version 3.0 or later
+// https://github.com/phi-amateur-radio-community/phi-basic-protocol
+// =====================================================================
+// Path /common/src/lib.rs
+// The common part of the project
+
 
